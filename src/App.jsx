2b10da1501fc1404645cb18c13ae3d49c9f939cd
okkonie <okko.niemikorpi1@gmail.com>
2025-09-06 20:49:45 +0300

    -

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="bg-gray-700 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">hi!</h1>
+    </div>
+  );
+}
+
+export default App;
